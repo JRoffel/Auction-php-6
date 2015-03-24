9eb562e3a36c2371d7a07609722bc1dfd4edffc0
JRoffel <jasonroffel@hotmail.nl>
2015-03-24 11:19:36 +0100

    A little something something
    LOLOLOLLO

--- index.action.php
+++ index.action.php
@@ -1,6 +1,6 @@
 <?php
 	$connection = new mysqli('localhost', 'root', '', 'auction');
-	$query = 'select * from item';
+	$query = 'select * from item left join';
 	$result = $connection->query($query);
 	$data = $result->fetch_all(MYSQLI_ASSOC);
 
